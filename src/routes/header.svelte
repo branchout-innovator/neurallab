7c35c0d6ccef57a8a503748a9c85d57d3bd6b5af
notken12 <kendotzhou@gmail.com>
2024-06-25 16:38:42 -0400

    fix href for prerender

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -10,7 +10,7 @@
 		<nav
 			class="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6"
 		>
-			<a href="##" class="text-foreground transition-colors hover:text-foreground"> Dashboard </a>
+			<a href="/" class="text-foreground transition-colors hover:text-foreground"> Dashboard </a>
 		</nav>
 		<div class="flex flex-1 items-center justify-between space-x-2 md:justify-end">
 			<ThemeToggle></ThemeToggle>
